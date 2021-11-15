3ed0ae17c07350178b4505b6d1f30d15112430d1
Frederick Bradley <fbradley21@icloud.com>
2021-11-14 22:37:50 -0600

    started word guessing game in Csharp

---
+++ GuessingGameCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GuessingGameCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "toyota";
+            string guess = "";
+
+
+            while(guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+            }
+
+            Console.Write("You Win!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
